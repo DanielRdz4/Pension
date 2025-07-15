aba462b3c3759b9a47ce18917798139124606619
Daniel <138172621+DanielRdz4@users.noreply.github.com>
2025-07-15 15:12:25 -0600

    Mejoras para la ejecución segura del programa

--- src/main.py
+++ src/main.py
@@ -1,17 +1,17 @@
-import sys
-from pathlib import Path
-
-# Añadir el directorio padre al path para poder importar utils
-sys.path.insert(0, str(Path(__file__).parent.parent))
-
 from utils.mod_preferencias import gestionar_preferencias, obt_preferencias
 from utils.calc_pension import calc_crecimiento, calc_pens_mensual
 
-gestionar_preferencias()
-preferencias = obt_preferencias()
-monto_final = calc_crecimiento(preferencias)
-pension_mensual = calc_pens_mensual(preferencias, monto_final)
+def main():
+    """
+    Función princial que ejecuta el cálculo de la pensión.
+    """
+    gestionar_preferencias()
+    preferencias = obt_preferencias()
+    monto_final = calc_crecimiento(preferencias)
+    pension_mensual = calc_pens_mensual(preferencias, monto_final)
 
-print(f"Inversión total: ${monto_final:,.2f}")
-print(f"Pensión mensual: ${pension_mensual:,.2f}")
+    print(f"Inverisón total: ${monto_final:,.2f}")
+    print(f"Pensión mensual: ${pension_mensual:,.2f}")
 
+if __name__ =="__main__":
+    main()
